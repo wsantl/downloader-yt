b52cf2454fb742f070c2940dcac53fb2916ed3d7
wsantl <wsantoslba@hotmail.com>
2023-12-14 08:50:16 -0300

    melhoria do código

--- downloader.py
+++ downloader.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, send_file
 from pytube import YouTube
 import os
 import time
@@ -10,30 +10,34 @@ DOWNLOADS_DIR = 'downloads'
 if not os.path.exists(DOWNLOADS_DIR):
     os.makedirs(DOWNLOADS_DIR)
 
-def download_video(url):
+def sanitize_filename(title):
+    """Substituir caracteres inválidos no nome do arquivo."""
+    return ''.join(char if char.isalnum() or char in (' ', '-') else '_' for char in title)
+
+def download_media(url, is_audio):
     try:
         yt = YouTube(url)
-        video = yt.streams.get_highest_resolution()
-
-        # Extrair o título do vídeo
-        video_title = yt.title
-
-        # Substituir caracteres inválidos no nome do arquivo
-        video_title = ''.join(char if char.isalnum() or char in (' ', '-') else '_' for char in video_title)
-
-        # Adicionar um timestamp para garantir unicidade
+        title = yt.title
+        sanitized_title = sanitize_filename(title)
         timestamp = int(time.time())
 
-        video_path = os.path.join(DOWNLOADS_DIR, f'{video_title}_{timestamp}.mp4')
+        if is_audio:
+            media = yt.streams.filter(only_audio=True).first()
+            extension = 'mp3'
+        else:
+            media = yt.streams.get_highest_resolution()
+            extension = 'mp4'
 
-        if not os.path.exists(video_path):
-            video.download(DOWNLOADS_DIR, 'temp_video')
-            os.rename(os.path.join(DOWNLOADS_DIR, 'temp_video'), video_path)
+        file_path = os.path.join(DOWNLOADS_DIR, f'{sanitized_title}_{timestamp}.{extension}')
 
-        return video_path, video_title
+        if not os.path.exists(file_path):
+            media.download(DOWNLOADS_DIR, f'temp_{extension}')
+            os.rename(os.path.join(DOWNLOADS_DIR, f'temp_{extension}'), file_path)
+
+        return file_path, title
 
     except Exception as e:
-        raise RuntimeError(f"Erro ao baixar o vídeo: {str(e)}")
+        raise RuntimeError(f"Erro ao baixar o {( 'áudio' if is_audio else 'vídeo')} : {str(e)}")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -41,64 +45,13 @@ def index():
         url = request.form['url']
 
         if 'audio' in request.form:
-            return redirect(url_for('download_audio', url=url))
+            audio_path, title = download_media(url, is_audio=True)
+            return send_file(audio_path, as_attachment=True, download_name=f"{title}.mp3")
         elif 'video' in request.form:
-            return redirect(url_for('download_video', url=url))
+            video_path, title = download_media(url, is_audio=False)
+            return send_file(video_path, as_attachment=True, download_name=f"{title}.mp4")
 
     return render_template('index.html')
-
-@app.route('/download_audio/<path:url>', methods=['GET', 'POST'])
-def download_audio(url):
-    try:
-        yt = YouTube(url)
-        audio = yt.streams.filter(only_audio=True).first()
-
-        # Extrair o título do vídeo
-        video_title = yt.title
-
-        # Substituir caracteres inválidos no nome do arquivo
-        video_title = ''.join(char if char.isalnum() or char in (' ', '-') else '_' for char in video_title)
-
-        # Adicionar um timestamp para garantir unicidade
-        timestamp = int(time.time())
-
-        audio_path = os.path.join(DOWNLOADS_DIR, f'{video_title}_{timestamp}.mp3')
-
-        if not os.path.exists(audio_path):
-            audio.download(DOWNLOADS_DIR, 'temp_audio')
-            os.rename(os.path.join(DOWNLOADS_DIR, 'temp_audio'), audio_path)
-
-        return audio_path, video_title
-
-    except Exception as e:
-        raise RuntimeError(f"Erro ao baixar o áudio: {str(e)}")
-
-@app.route('/download_video/<path:url>', methods=['GET', 'POST'])
-def download_video(url):
-    try:
-        yt = YouTube(url)
-        video = yt.streams.get_highest_resolution()
-
-        # Extrair o título do vídeo
-        video_title = yt.title
-
-        # Substituir caracteres inválidos no nome do arquivo
-        video_title = ''.join(char if char.isalnum() or char in (' ', '-') else '_' for char in video_title)
-
-        # Adicionar um timestamp para garantir unicidade
-        timestamp = int(time.time())
-
-        video_path = os.path.join(DOWNLOADS_DIR, f'{video_title}_{timestamp}.mp4')
-
-        if not os.path.exists(video_path):
-            video.download(DOWNLOADS_DIR, 'temp_video')
-            os.rename(os.path.join(DOWNLOADS_DIR, 'temp_video'), video_path)
-
-        return video_path, video_title
-
-    except Exception as e:
-        raise RuntimeError(f"Erro ao baixar o vídeo: {str(e)}")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
